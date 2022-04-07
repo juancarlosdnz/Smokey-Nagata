c3cb1aae5f05c59ae3528c5c1deb7bff47574c6a
Pablo Andreu <pablo.andreu.martin@gmail.com>
2022-04-07 13:24:36 +0200

    initial screen

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-window.onload = () => Game.init('canvas')
+const initialScreen = document.querySelector('.inital-screen')
+
+document.getElementById('start').onclick = () => {
+
+        Game.init('canvas')
+        initialScreen.classList.add('hidden')
+    }
+    //document.getElementById('start').onclick = () => className('hidden')
+    // document.getElementById("start").className =
+    //     document.getElementById("start").className.replace('hidden')
